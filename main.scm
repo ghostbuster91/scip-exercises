d558b40bb974a0e62f6f7518f4d67d8d4ec5736c
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-02-23 00:05:20 +0100

    FastExp2

--- main.scm
+++ main.scm
@@ -35,3 +35,48 @@
 
 (ff2 6)
 
+(define (expt b n)
+    (if (= n 0)
+      1
+      (* b (expt b (- n 1)))
+    )
+)
+
+(expt 4 2)
+
+(define (expt2 b n)
+  (expt-iter b n 1)
+)
+
+(define (expt-iter b counter product)
+  (if (= counter 0)
+    product
+    (expt-iter b (- counter 1) (* b product))
+  )
+)
+
+(expt2 4 10)
+
+(define (fast-exp b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-exp b (/ n 2))))
+        (else (* b (fast-exp b (- n 1))))
+  )
+)
+
+(fast-exp 2 4)
+(fast-exp 2 5)
+(fast-exp 2 6)
+(fast-exp 2 7)
+
+(define (fast-exp2 b n product)
+  (cond ((= n 0) product)
+        ((even? n) (fast-exp2 (square b) (/ n 2) product))
+        (else (fast-exp2 b (- n 1) (* b product)))
+  )
+)
+
+(fast-exp2 2 4 1)
+(fast-exp2 2 5 1)
+(fast-exp2 2 6 1)
+(fast-exp2 2 7 1)
