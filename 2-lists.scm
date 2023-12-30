aa054f68cac3e1bcd32152806d3201e2c25046ba
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-12-30 23:45:38 +0100

    flattening leaves

--- 2-lists.scm
+++ 2-lists.scm
@@ -1,3 +1,4 @@
+(define nil (list))
 
 (define (length2 items)
   (if (null? items)
@@ -23,11 +24,13 @@
 ;; 2.18
 
 (define (reverse2 list1)
-  (if (> (length2 list1) 1)
-    (cons (reverse2 (cdr list1)) (car list1))
-    (car list1)
+  (define (iter x answer)
+    (if (null? x)
+      answer
+      (iter (cdr x) (cons (car x) answer)))
     )
-  )
+ (iter list1 nil)
+)
 
 (reverse2 (list 1 12 35 55 66))
 
@@ -63,7 +66,6 @@
 
 ;; 2.22
 
-(define nil (list))
 
 (define (square-list items)
   (define (iter things answer)
@@ -96,3 +98,91 @@
 
 (for-each2 (lambda (x) (newline)(display x))
     (list 57 222 88))
+
+(define (count-leaves x)
+    (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x)) (count-leaves (cdr x))))
+        )
+  )
+
+(define tree1 (cons (list 1 2) (list 3 4)))
+
+(count-leaves tree1)
+(count-leaves (list tree1 tree1))
+(length2 tree1)
+
+;; 2.24
+(list 1 2 (list 3 4))
+
+;; (list 1 (list 2 (list 3 4)))
+;;  [1, [2 [3, 4]]]
+;;   .
+;; 1  .
+;;   2 .
+;;    3 4
+;;
+
+(count-leaves (list 1 2 (list 3 4)))
+(length2 (list 1 2 (list 3 4)))
+
+;; 2.25 
+(car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9))))))
+
+;; 2.26
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+;; append 
+;; (1 2 3 4 5 6)
+;; cons 
+;; ((1 2 3) 4 5 6)
+;; list 
+;; ((1 2 3) (4 5 6))
+
+(append x y)
+(cons x y)
+(list x y)
+
+;; 2.27
+
+(define (deep-reverse items)
+  (define (helper x)
+    (if (pair? x)
+      (deep-reverse x)
+      x
+    )
+  )
+  (define (iter x answer)
+    (if (null? x)
+      answer
+      (iter (cdr x) (cons (helper (car x)) answer)))
+    )
+ (iter items nil)
+) 
+
+
+(list (list 1 2) (list 3 4))
+(deep-reverse (list (list 1 2) (list 3 4)))
+(reverse2 (list (list 1 2) (list 3 4)))
+(reverse2 (list (list 1 2)))
+
+
+(deep-reverse (list (list 1 2) 3 4 (list 6 7)))
+
+;; 2.28
+
+(define (fringe items)
+    (define (go x acc)
+        (cond ((null? x) acc)
+                ((not (pair? x)) (cons x acc))
+                (else (append (go (car x) acc) (go (cdr x) acc)))
+                )
+      )
+    (go items nil)
+  )
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+(fringe (list x x))
